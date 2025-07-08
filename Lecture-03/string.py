1f1291067a774fa653ca5266a06ab6da03d26df2
HAthaway1420 <pattarakorn.mook@gmail.com>
2025-07-08 15:35:21 +0700

    03

---
+++ Lecture-03/string.py
@@ -0,0 +1,22 @@
+string1 = "mary"
+string2 = "mark"
+
+if string1 == string2 :
+    print(f'"{string1}") and "{string2}" are equal.')
+else:
+    print(f'"{string1}") and "{string2}" are equal.')
+ 
+if string1 < string2:
+    print(f'"{string1}"comes before "{string2}"in
+    lexicographical oder.')
+elif string1 > string2: 
+    print(f'"{string1}"comes before "{string2}"in
+    lexicographical oder.')
+
+if string1. lower() == string2.lower():
+    print(f'"{string1}" and "{stringprint(f'"{string1}"comes before "{string2}"in
+    lexicographical oder.')2}" are equal when case is 
+        ignored.')
+else:
+    print(f'"{string1}" and "{string2}" are equal when case is 
+        ignored.')
