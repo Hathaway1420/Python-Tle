bb4739ac7c9fcaa3def7a6e080fa50e5123792ef
HAthaway1420 <pattarakorn.mook@gmail.com>
2025-07-29 16:29:48 +0700

    06

--- Lecture-06/barista.py
+++ Lecture-06/barista.py
@@ -9,8 +9,8 @@ def main():
 
     pay_rate = float(input('Enter the hourly pay rate : '))
 
-    for index in range (NUM_EMPLOYEES):
-    gross_pay = hours[index] * pay_rate
+    for index in range(NUM_EMPLOYEES):
+        gross_pay = hours[index] * pay_rate
     print('Gross pay for employee',index+1, ':$', format(gross_pay,',.2f'), sep='')
     
 main()
