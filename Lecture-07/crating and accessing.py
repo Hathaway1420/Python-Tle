db7e8cb58eeaa6ff140d7c148ab568173c37d858
HAthaway1420 <pattarakorn.mook@gmail.com>
2025-08-05 16:17:01 +0700

    07

---
+++ Lecture-07/crating and accessing.py
@@ -0,0 +1,14 @@
+studen = {"name": "alice", "age":25, "grade":"A"}
+print(studen)
+
+student =dict (Name ="alice", age = 25, grade = "A")
+print(student)
+
+student1 = dict([("name", "Alice"),("age",25),("grade","A")])
+print(student1)
+
+student2 = {"name":  "alice", "age": 25,"grade":"A"}
+
+print (student2["name"])
+print(student2["age"])
+print(student2["grade"])
